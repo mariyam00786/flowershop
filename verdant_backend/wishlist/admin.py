4c704c0c877de66e5f400ca3cb4bcf3ca2b13893
mariyam00786 <mariyam00786@users.noreply.github.com>
2026-06-02 12:26:24 +0530

    auto: update 2026-06-02 12:26:24

--- verdant_backend/wishlist/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import WishlistItem
-
-
-@admin.register(WishlistItem)
-class WishlistItemAdmin(admin.ModelAdmin):
-    list_display = ('user', 'plant', 'added_at')
-    search_fields = ('user__email', 'plant__name')
-    list_filter = ('added_at',)
--- verdant_backend/wishlist/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import WishlistListView, WishlistAddView, WishlistRemoveView
-
-urlpatterns = [
-    path('', WishlistListView.as_view(), name='wishlist-list'),
-    path('add/', WishlistAddView.as_view(), name='wishlist-add'),
-    path('remove/<int:pk>/', WishlistRemoveView.as_view(), name='wishlist-remove'),
-]
